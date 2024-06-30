4358b4b65a419782fa9cb040cbdf78fda030867e
Moctar Diallo <moctarjallo@gmail.com>
2024-06-30 15:29:54 +0000

    Add 'save_bulletin' poc

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from PyPDF2 import PdfReader
+import os
+from PyPDF2 import PdfWriter, PdfReader, PdfMerger
 
 def read_bulletins():
     with open("bulletins_paie.pdf", 'rb') as infile:
@@ -15,7 +16,29 @@ def read_bulletins():
 def get_bulletin(matricule, bulletins):
     return [bulletin for bulletin in bulletins if bulletin.get(matricule)][0]
 
+def save_bulletin(file_name):
+    if not os.path.isfile(file_name):
+        print(f"The file {file_name} does not exist.")
+    else:
+        inputpdf = PdfReader(open(file_name, "rb"))
+
+        for i in range(0, 6):
+            writer = PdfWriter()
+            writer.add_page(inputpdf.pages[i])
+            with open("%s-page%s.pdf" % (file_name, i), "wb") as output_pdf:
+                writer.write(output_pdf)
+
+        # Merge the split PDF files
+        merge_pdf = PdfMerger()
+
+        for i in range(0, 6):
+            merge_pdf.append(open("%s-page%s.pdf" % (file_name, i), "rb"))
+
+        with open("merged_%s" % file_name, "wb") as output_pdf:
+            merge_pdf.write(output_pdf)
+
 if __name__ == '__main__':
-    bulletins = read_bulletins()
-    b = get_bulletin(30030654, bulletins)
-    print(b)
+    # bulletins = read_bulletins()
+    # b = get_bulletin(30030654, bulletins)
+    save_bulletin("bulletins_paie.pdf")
+    # print(b)
