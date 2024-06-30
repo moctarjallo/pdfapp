d47f27f182f934def648b71f9e17823958a17b6e
Moctar Diallo <moctarjallo@gmail.com>
2024-06-30 16:17:38 +0000

    Implement 'get_matricule'

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import os
 from PyPDF2 import PdfWriter, PdfReader, PdfMerger
 
-def read_bulletins():
-    with open("bulletins_paie.pdf", 'rb') as infile:
-        pages = PdfReader(infile).pages
 
-        def parse_bulletin(page):
-            page_text = page.extract_text()
-            ligne_matricule = page_text.split('\n')[6]
-            matricule = int(ligne_matricule.split()[2])
-            return {matricule: page}
-
-        return [parse_bulletin(page) for page in pages]
+def get_matricule(page):
+    page_text = page.extract_text()
+    ligne_matricule = page_text.split('\n')[6]
+    matricule = int(ligne_matricule.split()[2])
+    return matricule
 
 def get_bulletin(matricule, bulletins):
     return [bulletin for bulletin in bulletins if bulletin.get(matricule)][0]
@@ -19,15 +14,16 @@ def get_bulletin(matricule, bulletins):
 def save_bulletin(page, file_name):
     writer = PdfWriter()
     writer.add_page(page)
-    with open("%s-page.pdf" % (file_name), "wb") as output_pdf:
+    with open(f"{file_name}.pdf", "wb") as output_pdf:
         writer.write(output_pdf)
 
 def save_bulletins(file_name):
     with open(file_name, "rb") as in_f:
         pages = PdfReader(in_f).pages
-        for i in range(0, 2):
+        for i in range(0, 3):
             page = pages[i]
-            save_bulletin(page, f"{file_name}-{i}")
+            matricule = get_matricule(page)
+            save_bulletin(page, f"{matricule}")
 
 def merge_pdf(file_name):
     # Merge the split PDF files
@@ -39,10 +35,9 @@ def merge_pdf(file_name):
     with open("merged_%s" % file_name, "wb") as output_pdf:
         merge_pdf.write(output_pdf)
 
-if __name__ == '__main__':
-    # bulletins = read_bulletins()
-    # b = get_bulletin(30030654, bulletins)
+def main():
     file_name = "bulletins_paie.pdf"
     save_bulletins(file_name)
-    # merge_pdf(file_name)
-    # print(b)
+
+if __name__ == '__main__':
+    main()
