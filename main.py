ffcd8888294283712d42728282378bbcab78a04d
Moctar Diallo <moctarjallo@gmail.com>
2024-09-24 14:38:48 +0000

    Send payment to specific phone number

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 
+import pandas as pd
+
 from payment import Bulletins
 from messaging import Media
 
@@ -31,20 +33,25 @@ def send_pdf_use_case():
         "vHOxtE94pSdsiG6rIuulfKmxbltfAaS"
     ).send("notification_conge.pdf", "221771332916", "Bon conges!")
 
-def send_bulletin_use_case():
+def send_bulletin_to_num_use_case():
     file_name = "bulletins_paie.pdf"
     bulletins = Bulletins(file_name)
-    matricule = 30030944
-    b = bulletins.get_bulletin(matricule)
-    bulletins.save_bulletin(b, f"{matricule}.pdf")
-    Media(458857697302383, 
-        "EAAMfJJStAz4BOZCknQ0VUznrDXTEKNPKwXJhnae2V1HfTv3"
-        "ghtgM4NEDfQI4JuxfVDofOUeM7qojXC6NvczKlaXE6T7EY8Q"
-        "YZBggn4TvWC8VxopgpFdZCc4uv8fjwUnodwSujMMEG4oZARR"
-        "NW4jt0D2O0i22wotbLIq0HZCKjp7KTHqohEZBdTHm2Bavoh8"
-        "vHOxtE94pSdsiG6rIuulfKmxbltfAaS"
-    ).send(f"{matricule}.pdf", "221771332916")
-    os.remove(f"{matricule}.pdf")
+    telephone = '+221778577500'
+    df = pd.read_excel('recipients.xlsx')
+    matricules = df[df['Telephone'].str.replace(" ", "") == telephone]['Matricule'].tolist()
+    for matricule in matricules:
+        b = bulletins.get_bulletin(matricule)
+        bulletins.save_bulletin(b, f"{matricule}.pdf")
+    media = Media(458857697302383, 
+        "EAAMfJJStAz4BOyGj6je0WNiTYkeZAmNGutiDfy89K5q"
+        "YGfViUNZA6DUpV6nzzZCRr748Ew6woUZCGSwn9FAsxSn"
+        "flB7eO9NXEkNpDdjJh5jtHRlg7izrGK5mXIMVWzdZCWz"
+        "tSJb8PEHNn9d5Ba8R27uimuLfnOAxqWlDeIBuaqL1vzM"
+        "CCXs1HZCVDjVcAm6k5t0Qxb39XUuWrSTUIplEAxflg4yHYZD"
+    )
+    for matricule in matricules:
+        media.send(f"{matricule}.pdf", telephone)
+        os.remove(f"{matricule}.pdf")
 
 if __name__ == '__main__':
     # save_bulletins_use_case()
