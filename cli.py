7d1af492c8d8b55b2078a75de2ec6dca48bf5a07
Moctar Diallo <moctarjallo@gmail.com>
2024-09-26 03:20:22 +0000

    Specifications cli commands

---
+++ cli.py
@@ -0,0 +1,6 @@
+"""
+sagsa generate --doc="notification_conge.pdf" --mode="leave" --params="recipients.xlsx"
+sagsa send --doc="notification_conge" --params="recipients.xlsx"
+sagsa delete --doc="notification_conge"
+"""
+
