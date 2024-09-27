e402bbe22c42485f2b0d0f4f519be7894dc7d732
Moctar Diallo <moctarjallo@gmail.com>
2024-09-27 15:49:55 +0000

    Upgrade api and cli

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ from messaging import Media
 import pandas as pd
 
 PHONE_NUMBER_ID = 458857697302383
-ACCESS_TOKEN = "EAAMfJJStAz4BO4xyKx5JnEiwVsX2HTtaLtyZARDqjwOlV0B1veYMYmp2DTzO6kdnjKcphvYo6GIHZCtTZApYgVfdxo7B9q5f4OQJ3oV9ZAjNt4dR98fIwfyeb4U3Uc9HzopIzqZCkxZBJ8gqR3g0t4OjxZB4kNMSk9IAFzh36uosqVEv4Sz4u72XcOPE58d1i991FZBgUksbA7aKzgSyL8CWC8hZAbdcZD"
+ACCESS_TOKEN = "EAAMfJJStAz4BOy2mlMtNRDYdFZBsBszaZBOSfZBGfgh3XZCGkmuNZAZAB5Wa2kxpLonMo3hFL2exthGN6wCnR9weV7IetuiLe5zHjZCtayfZBPTDKRVNgNlc2p7y2lKF6ySjpsZBCEQtx42ItJn7meefhc7hpx2nJddB6bHF31ad4XzHS6wjxAKN1Fj4t2TSWmJXA4QOrY2LqeHOTbg47DFitS6qQnmYZD"
 
 """
 Add these 3 general and separated use cases:
@@ -18,7 +18,7 @@ backend:
                 RETURNS progress bar (tqdm?)
 """
 
-def generate(pdf_path, excel_path):
+def generate_folder(pdf_path, excel_path):
     folder_name = Path(pdf_path).stem
     folder = Path(folder_name)
     if not folder.exists():
@@ -29,20 +29,34 @@ def generate(pdf_path, excel_path):
         b = leaves.get_page_by_matricule(matricule)
         file = Path(f"{folder}/{matricule}.pdf")
         leaves.save_to_pdf(b, file)
+    return folder_name
 
-def delete(folder_name):
+def delete_folder(folder_name):
     folder = Path(folder_name)
     for file in folder.iterdir():
         file.unlink()
     folder.rmdir()
 
-def send(folder_name, excel_path):
+def send_folder(folder_name, excel_path):
     media = Media(PHONE_NUMBER_ID, ACCESS_TOKEN)
     df = pd.read_excel(excel_path)
     for matricule, telephone in zip(df['Matricule'], df['Telephone'].str.replace(' ', '')):
         media.send(f"{folder_name}/{matricule}.pdf", telephone)
 
+def send(pdf_path, excel_path, generate=True, delete=True):
+    """
+    pdf_path: either a path to a pdf file or a folder containing individual pdfs
+    """
+    if generate:
+        folder_name = generate_folder(pdf_path, excel_path)
+    else:
+        folder_name = pdf_path
+    send_folder(folder_name, excel_path)
+    if delete:
+        delete_folder(folder_name)
+
 if __name__ == '__main__':
-    # generate('C:\\Users\\HP\kajande\\frontend\\python\\api\\pdfapp\\notification_conge.pdf', 'recipients.xlsx')
-    send('notification_conge', 'recipients.xlsx')
-    # delete('notification_conge')
+    # generate_folder('C:\\Users\\HP\kajande\\frontend\\python\\api\\pdfapp\\notification_conge.pdf', 'recipients.xlsx')
+    # send_folder('notification_conge', 'recipients.xlsx')
+    # delete_folder('notification_conge')
+    send('notification_conge.pdf', 'recipients.xlsx')
--- cli.py
+++ cli.py
@@ -8,15 +8,30 @@ sagsa send --doc="notification_conge" --params="recipients.xlsx"
 sagsa delete --doc="notification_conge"
 """
 
-parser = argparse.ArgumentParser()
+def run():
+    parser = argparse.ArgumentParser()
 
-parser.add_argument('command', choices=['generate', 'send', 'delete'])
-parser.add_argument('--pdf')
-parser.add_argument('--excel')
+    parser.add_argument('command', choices=['generate_folder', 'send_folder', 'delete_folder', 'send'])
+    parser.add_argument('--pdf')
+    parser.add_argument('--folder')
+    parser.add_argument('--excel')
+    parser.add_argument('--generate', type=int, default=1)
+    parser.add_argument('--delete', type=int, default=1)
 
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
 
-if args.command == 'generate':
-    api.generate(args.pdf, args.excel)
+    if args.command == 'generate_folder':
+        api.generate_folder(args.pdf, args.excel)
+    elif args.command == 'send_folder':
+        api.send_folder(args.folder, args.excel)
+    elif args.command == 'delete_folder':
+        api.delete_folder(args.folder)
+    elif args.command == 'send':
+        api.send(args.pdf, args.excel, args.generate, args.delete)
+
+
+
+if __name__ == '__main__':
+    run()
