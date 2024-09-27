3cbff63fd36ea6fb75cfbab47ddba853975a00b1
Moctar Diallo <moctarjallo@gmail.com>
2024-09-27 10:00:05 +0000

    Implemented generate command

--- cli.py
+++ cli.py
@@ -1,6 +1,22 @@
+import argparse
+
+import api
+
 """
 sagsa generate --doc="notification_conge.pdf" --mode="leave" --params="recipients.xlsx"
 sagsa send --doc="notification_conge" --params="recipients.xlsx"
 sagsa delete --doc="notification_conge"
 """
 
+parser = argparse.ArgumentParser()
+
+parser.add_argument('command', choices=['generate', 'send', 'delete'])
+parser.add_argument('--pdf')
+parser.add_argument('--excel')
+
+
+args = parser.parse_args()
+
+
+if args.command == 'generate':
+    api.generate(args.pdf, args.excel)
