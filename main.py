b8f8b029e5b28f20db6e9f7163cde24a7257b202
Moctar Diallo <moctarjallo@gmail.com>
2024-09-29 20:47:03 +0000

    Start crawling developers.facebook.com

--- main.py
+++ main.py
@@ -1,122 +1,26 @@
-import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
-import pandas as pd
+import time
 
-from payment import Bulletins, Leave
-from messaging import Media
+driver = webdriver.Chrome()
+driver.get("https://developers.facebook.com/apps/878666903782206/whatsapp-business/wa-dev-console/?business_id=1620644888498053")
 
-ACCESS_TOKEN = "EAAMfJJStAz4BO7hIDdqCy4BVuc4fb59EYQKZCSgoNdKFwnZCs4PTjfHOSzCWCN3892NsTZCA21uKzQOmaXZB5YdSPXhgEmXa0ZAHVdp2YIHBZBk0ZBN2NVheNy6Gfu1hMftUklFYrXFbc8ZA6szWr733RPcFXG9EgozOIqwPZBR8R66CyG7d6uD7CMfworXCdX4QBHPZA8lfEK1rUzK3LKRgaiR9ZAqaocZD"
+# assert "Python" in driver.title
+elem = driver.find_element(By.ID, "email")
+elem.send_keys("moctarjallo@gmail.com")
 
-def save_to_pdfs_use_case():
-    file_name = "bulletins_paie.pdf"
-    bulletins = Bulletins(file_name, 3)
-    bulletins.save_to_pdfs()
+elem = driver.find_element(By.ID, "pass")
+elem.send_keys("Dj1nk@n!")
 
-def get_page_by_matricule_use_case():
-    file_name = "bulletins_paie.pdf"
-    bulletins = Bulletins(file_name)
-    matricule = 30030944
-    b = bulletins.get_page_by_matricule(matricule)
-    bulletins.save_to_pdf(b, f"{matricule}.pdf")
+elem.send_keys(Keys.RETURN)
 
-def get_page_use_case():
-    file_name = "bulletins_paie.pdf"
-    page_num = 20
-    bulletins = Bulletins(file_name)
-    b = bulletins.get_page_by_num(page_num)
-    bulletins.save_to_pdf(b, f"{page_num}.pdf")
+elem = driver.find_element(By.ID, "js_4d")
+elem.click()
 
-def send_pdf_use_case():
-    Media(458857697302383, 
-        "EAAMfJJStAz4BOZCknQ0VUznrDXTEKNPKwXJhnae2V1HfTv3"
-        "ghtgM4NEDfQI4JuxfVDofOUeM7qojXC6NvczKlaXE6T7EY8Q"
-        "YZBggn4TvWC8VxopgpFdZCc4uv8fjwUnodwSujMMEG4oZARR"
-        "NW4jt0D2O0i22wotbLIq0HZCKjp7KTHqohEZBdTHm2Bavoh8"
-        "vHOxtE94pSdsiG6rIuulfKmxbltfAaS"
-    ).send("notification_conge.pdf", "221771332916", "Bon conges!")
+time.sleep(60)
+# elem.clear()
+# assert "No results found." not in driver.page_source
+driver.close()
 
-def send_bulletin_to_num_use_case():
-    file_name = "bulletins_paie.pdf"
-    bulletins = Bulletins(file_name)
-    telephone = '+221778577500'
-    df = pd.read_excel('recipients.xlsx')
-    matricules = df[df['Telephone'].str.replace(" ", "") == telephone]['Matricule'].tolist()
-    for matricule in matricules:
-        b = bulletins.get_page_by_matricule(matricule)
-        bulletins.save_to_pdf(b, f"{matricule}.pdf")
-    media = Media(458857697302383, ACCESS_TOKEN)
-    for matricule in matricules:
-        media.send(f"{matricule}.pdf", telephone)
-        os.remove(f"{matricule}.pdf")
-
-
-def send_leave_to_num_use_case():
-    file_name = "notification_conge.pdf"
-    leaves = Leave(file_name)
-    telephone = '+221778577500'
-    df = pd.read_excel('recipients.xlsx')
-    matricules = df[df['Telephone'].str.replace(" ", "") == telephone]['Matricule'].tolist()
-    for matricule in matricules:
-        b = leaves.get_page_by_matricule(matricule)
-        leaves.save_to_pdf(b, f"{matricule}.pdf")
-    media = Media(458857697302383, ACCESS_TOKEN)
-    for matricule in matricules:
-        media.send(f"{matricule}.pdf", telephone)
-        os.remove(f"{matricule}.pdf")
-
-def send_bulletin_with_excel_use_case():
-    file_name = "bulletins_paie.pdf"
-    bulletins = Bulletins(file_name)
-    df = pd.read_excel('recipients.xlsx')
-    matricules = df['Matricule']
-    for matricule in matricules:
-        b = bulletins.get_page_by_matricule(matricule)
-        bulletins.save_to_pdf(b, f"{matricule}.pdf")
-    media = Media(458857697302383, 
-        "EAAMfJJStAz4BOyGj6je0WNiTYkeZAmNGutiDfy89K5q"
-        "YGfViUNZA6DUpV6nzzZCRr748Ew6woUZCGSwn9FAsxSn"
-        "flB7eO9NXEkNpDdjJh5jtHRlg7izrGK5mXIMVWzdZCWz"
-        "tSJb8PEHNn9d5Ba8R27uimuLfnOAxqWlDeIBuaqL1vzM"
-        "CCXs1HZCVDjVcAm6k5t0Qxb39XUuWrSTUIplEAxflg4yHYZD"
-    )
-    for matricule, telephone in zip(df['Matricule'], df['Telephone'].str.replace(' ', '')):
-        media.send(f"{matricule}.pdf", telephone)
-        os.remove(f"{matricule}.pdf")
-
-
-def send_leave_with_excel_use_case():
-    file_name = "notification_conge.pdf"
-    leaves = Leave(file_name)
-    df = pd.read_excel('recipients.xlsx')
-    matricules = df['Matricule']
-    for matricule in matricules:
-        b = leaves.get_page_by_matricule(matricule)
-        leaves.save_to_pdf(b, f"{matricule}.pdf")
-    media = Media(458857697302383, ACCESS_TOKEN)
-    for matricule, telephone in zip(df['Matricule'], df['Telephone'].str.replace(' ', '')):
-        media.send(f"{matricule}.pdf", telephone)
-        os.remove(f"{matricule}.pdf")
-
-
-"""
-frontend:
-    1. Add 2 columns to excel file: 
-        - Document: specifies the path to the pdf document to be processed
-        - Delete: boolean specifying if the corresponding generated file should be deleted
-    2. Add buttons:
-        - GENERATE
-        - SEND
-        - DELETE
-        - GENERATE - SEND - DELETE
-"""
-
-
-if __name__ == '__main__':
-    # save_to_pdfs_use_case()
-    # get_page_by_matricule_use_case()
-    # get_page_use_case()
-    # send_pdf_use_case()
-    # send_bulletin_to_num_use_case()
-    # send_leave_to_num_use_case()
-    send_bulletin_with_excel_use_case()
-    # send_leave_with_excel_use_case()
